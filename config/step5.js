d5936c1569b783f19e1166e9961b266f0e69f2e5
azuazar29 <azarudheen.sm@gmail.com>
2022-04-18 18:48:57 +0530

    after changes

--- config/step5.js
+++ config/step5.js
@@ -553,7 +553,7 @@ function start1(
       compressor_needed: display_installed_rooms.length.toString(),
       FCU_needed: roomsArray.length.toString(),
       condenserIDs: condenserIDs,
-      product_subscription_cost: subCost,
+      product_subscription_cost: subCost.toString(),
       product_description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     };
